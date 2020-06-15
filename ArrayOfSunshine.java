3d0c9d07c12fd3b24b8e9dd542d6a65ba24ba1df
Simran Srivastava <52354644+simransrivastava01@users.noreply.github.com>
2020-06-15 14:53:03 +0530

    Day7

---
+++ ArrayOfSunshine.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+        int l=n;
+        for(int i=0;i<n/2;i++)
+        {
+            int temp=arr[i];
+            arr[i]=arr[l-1-i];
+            arr[l-1-i]=temp;
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+        scanner.close();
+    }
+}
