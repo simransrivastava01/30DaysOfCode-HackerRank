1aa358e9e811157ea3729208c6a4fa221419cfd6
Simran Srivastava <52354644+simransrivastava01@users.noreply.github.com>
2020-06-24 14:02:31 +0530

    Day16

---
+++ Exception.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        try{
+             String S = in.next();
+            System.out.print(Integer.parseInt(S));
+        }
+        catch(Exception e){
+            System.out.print("Bad String");
+        }
+    }
+}
